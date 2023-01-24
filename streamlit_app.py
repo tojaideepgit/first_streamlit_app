eb5090978fab89582f924961c21f4750f93916d2
tojaideepgit <123513891+tojaideepgit@users.noreply.github.com>
2023-01-24 17:24:54 -0500

    Update streamlit_app.py
    Added index

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,8 @@ streamlit.text('Spinach smoothie')
 streamlit.text('hardboiled eggs')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
+
