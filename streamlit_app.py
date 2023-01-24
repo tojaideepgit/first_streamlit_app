f4fed28e3e11e3246790af061adc9ab80d61432a
tojaideepgit <123513891+tojaideepgit@users.noreply.github.com>
2023-01-24 17:16:45 -0500

    Update streamlit_app.py
    Added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parent new healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Spinach smoothie')
+streamlit.text('hardboiled eggs')
