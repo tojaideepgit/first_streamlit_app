0a44e9d98f9eae9bd9942bcb8cece2979a4d937f
tojaideepgit <123513891+tojaideepgit@users.noreply.github.com>
2023-01-24 17:21:03 -0500

    Update streamlit_app.py
    imported pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parent new healthy Dinner')
 
@@ -6,3 +7,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry oatmeal')
 streamlit.text('Spinach smoothie')
 streamlit.text('hardboiled eggs')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
